ebb986554a105f526414f37c7e4242d200e33a90
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-10-24 15:12:31 +0300

    BranchA: Implemented FuncA with n elements

--- func.cpp
+++ func.cpp
@@ -1,6 +1,20 @@
-#include "func.h"
+#include <cmath>
 
-double FuncClass::FuncA(int n) {
-    return 0;  // Поки що просто повертає 0
+class FuncClass {
+public:
+    double FuncA(int n, double x);
+};
+
+double FuncClass::FuncA(int n, double x) {
+    double sum = 0.0;
+
+    for (int i = 0; i < n; ++i) {
+        double term = (std::pow(-1, i) * std::tgamma(2 * i + 1)) / 
+                      ((1 - 2 * i) * std::tgamma(i + 1) * std::pow(4, i)) * std::pow(x, i);
+        
+        sum += term;
+    }
+
+    return sum;
 }
 
