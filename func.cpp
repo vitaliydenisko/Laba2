c359e6f8cb4f2e27d828226c371c8c6c980191d5
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-10-24 14:53:54 +0300

    Hotfix: FuncA returns sum of first 3 elements

--- func.cpp
+++ func.cpp
@@ -1,6 +1,21 @@
-#include "func.h"
+#include <cmath> 
 
-double FuncClass::FuncA(int n) {
-    return 0;  // Поки що просто повертає 0
+class FuncClass {
+public:
+    double FuncA(int n, double x);
+};
+
+double FuncClass::FuncA(int n, double x) {
+    double sum = 0.0;
+
+    // Обмежуємо кількість ітерацій до 3
+    for (int i = 0; i < 3; ++i) { // ітеруємо лише 3 рази
+        double term = (std::pow(-1, i) * std::tgamma(2 * i + 1)) /
+                      ((1 - 2 * i) * std::tgamma(i + 1) * std::pow(4, i)) * std::pow(x, i);
+        sum += term;
+    }
+
+    return sum;
 }
+
 
