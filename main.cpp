58c8b73d1c226c840627904a3443e4c07bab9bcb
vitaliydenisko <132932405+vitaliydenisko@users.noreply.github.com>
2024-11-20 15:53:28 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 #include "func.h"
 
 int main() {
-    FuncClass obj;
-    std::cout << "Result: " << obj.FuncA() << std::endl;
+    FuncClass funcObj;
+
+    int n;
+    double x;
+
+    // Введення параметрів
+    std::cout << "Enter the number of terms (n): ";
+    std::cin >> n;
+
+    std::cout << "Enter the value of x: ";
+    std::cin >> x;
+
+    // Виклик функції
+    double result = funcObj.FuncA(n, x);
+
+    // Вивід результату
+    std::cout << "Result of the series calculation: " << result << std::endl;
+
     return 0;
 }
-
