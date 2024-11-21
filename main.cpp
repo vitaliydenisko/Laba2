964f796053f44c9a0e8a0f2fcde04977380f8c81
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-11-21 12:32:37 +0200

    update coding func.cpp and main.cpp

--- main.cpp
+++ main.cpp
@@ -7,18 +7,4 @@ int main() {
     int n;
     double x;
 
-    // Запитуємо користувача на введення параметрів
-    std::cout << "Enter the number of terms (n): ";
-    std::cin >> n;
-
-    std::cout << "Enter the value of x: ";
-    std::cin >> x;
-
-    // Викликаємо функцію FuncA з переданими аргументами
-    double result = obj.FuncA(n, x);
-
-    // Виводимо результат
-    std::cout << "Result: " << result << std::endl;
-
-    return 0;
-}
+    // 
