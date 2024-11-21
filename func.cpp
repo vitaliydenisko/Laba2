dd6d1feb69684c21fbcdb3d62099601833df71f5
vitaliydenisko <vip.vitalii.2004@ukr.net>
2024-11-21 12:22:26 +0200

    Fix func.cpp and func.h

--- func.cpp
+++ func.cpp
@@ -1,20 +1,15 @@
 #include "func.h"
 #include <cmath>
-#include <algorithm> // Для std::min
+#include <algorithm>
 
-// Реалізація функції повинна відповідати оголошенню в func.h
 double FuncClass::FuncA(int n, double x) {
     double sum = 0.0;
-
-    // Обмежуємо кількість членів до 3 або n, що менше
     int limit = std::min(n, 3);
     for (int i = 0; i < limit; ++i) {
         double term = (std::pow(-1, i) * std::tgamma(2 * i + 1)) /
                       ((1 - 2 * i) * std::tgamma(i + 1) * std::pow(4, i)) * std::pow(x, i);
         sum += term;
     }
-
     return sum;
 }
-
 
